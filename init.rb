f2c95446ae06850850200ec3503fabac93abe0dc
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-04-21 14:45:56 +0000

    dont load rubygems
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/gems@1058 567b1171-46fb-0310-a4c9-b4bef9110e78

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
-gems = Dir["vendor/**"]
+standard_dirs = ['rails', 'plugins']
+gems          = Dir["vendor/**"]
 if gems.any?
-  require 'rubygems' unless Object.const_defined?(:Gem)
   gems.each do |dir|
-    next if ['rails', 'plugins'].include?(File.basename(dir))
+    next if standard_dirs.include?(File.basename(dir))
     lib = File.join(RAILS_ROOT, dir, 'lib')
     $LOAD_PATH.unshift(lib) if File.directory?(lib)
   end
